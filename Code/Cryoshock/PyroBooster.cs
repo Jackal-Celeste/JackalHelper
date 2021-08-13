a67f5cac520a02fab62c33dc5683dc1f762c903c
Jackal-Celeste <73126893+Jackal-Celeste@users.noreply.github.com>
2021-08-13 14:47:54 -0400

    Update PyroBooster.cs
    Fixed particles overriding vanilla boosters (1/2)

--- Code/Cryoshock/PyroBooster.cs
+++ Code/Cryoshock/PyroBooster.cs
@@ -18,14 +18,14 @@ namespace Celeste.Mod.JackalHelper.Entities
 		public PyroBooster(Vector2 position) : base(position, true)
 		{
 			boostData = new DynData<Booster>(this);
-			base.Remove(boostData.Get<Sprite>("sprite"));
-			sprite = JackalModule.spriteBank.Create("boosterLava");
-			boostData.Set<Sprite>("sprite", JackalModule.spriteBank.Create("boosterLava"));
-			base.Add(boostData.Get<Sprite>("sprite"));
-			boostData.Get<BloomPoint>("bloom").Alpha = 0.5f;
-			boostData.Get<ParticleType>("particleType").Color = Color.OrangeRed;
-			boostData.Get<ParticleType>("particleType").Color2 = Color.OrangeRed;
-			base.Ch9HubBooster = true;
+            base.Remove(boostData.Get<Sprite>("sprite"));
+            sprite = JackalModule.spriteBank.Create("boosterLava");
+            boostData.Set<Sprite>("sprite", JackalModule.spriteBank.Create("boosterLava"));
+            base.Add(boostData.Get<Sprite>("sprite"));
+            boostData.Get<BloomPoint>("bloom").Alpha = 0.5f;
+            ParticleType tempParticle = boostData.Get<ParticleType>("particleType");
+            boostData.Set<ParticleType>("particleType", new ParticleType(tempParticle) { Color = Color.OrangeRed, Color2 = Color.OrangeRed });
+            base.Ch9HubBooster = true;
 
 		}
 		public PyroBooster(EntityData data, Vector2 offset)
